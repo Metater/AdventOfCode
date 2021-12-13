f273316f0e3b00b034c237c2fa5a288e7f37b729
Connor <61102529+Metater@users.noreply.github.com>
2021-12-13 12:23:21 -0500

    Work on day 13 part 1

--- AdventOfCode2021/Days/Day13P1.cs
+++ AdventOfCode2021/Days/Day13P1.cs
@@ -1,4 +1,7 @@
-namespace AdventOfCode2021.Days;
+using System.Collections.Generic;
+using System;
+
+//namespace AdventOfCode2021.Days;
 
 public class Day13P1 : Day
 {
@@ -7,8 +10,38 @@ public class Day13P1 : Day
 
     }
 
+    bool[,] paper;
+
     public override void Run()
     {
+        List<(int, int)> points = new();
+        int line = 0;
+		int maxX = -1;
+        int maxY = -1;
+        while (input[line] != "")
+        {
+            string[] split = input[line].Split(',');
+			int x = int.Parse(split[0]);
+			int y = int.Parse(split[1]);
+			if (x > maxX) maxX = x;
+            if (y > maxY) maxY = y;
+            points.Add((x, y));
+            line++;
+        }
+        paper = new bool[maxX + 1, maxY + 1];
+		foreach ((int x, int y) in points)
+		{
+			paper[x, y] = true;
+		}
+        Console.WriteLine($"Paper Size: ({paper.GetLength(0)}, {paper.GetLength(1)})");
 
+		for (int y = 0; y < paper.GetLength(1); y++)
+		{
+			for (int x = 0; x < paper.GetLength(0); x++)
+			{
+				Console.Write(paper[x, y] ? "#" : ".");
+			}
+			Console.WriteLine();
+		}
     }
 }
